364545a56126685228bb4fdab4450f9a1aeb95b9
igorcol <igor.colombini@gmail.com>
2025-03-24 15:33:50 -0300

    basic auth configuration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,37 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
-  <>
-    {/* OVERVIEW */}
-    <BookOverview 
-      {...{
-        ...sampleBooks[0],
-        color: sampleBooks[0].coverColor,
-        video: sampleBooks[0].videoUrl,
-      }}
-    />
+const Home = async () => {
+  const result = await db.select().from(users);
+  
+  console.log(JSON.stringify(result, null, 2))
 
-    {/* BOOK LIST */}
-    <BookList 
-      title='Latest Books'
-      books={sampleBooks.map(book => ({
-        ...book,
-        color: book.coverColor,
-        video: book.videoUrl,
-      }))}
-      containerClassname="mt-28"
-    />
-  </>
-);
+  return (
+    <>
+      {/* OVERVIEW */}
+      <BookOverview
+        {...{
+          ...sampleBooks[0],
+          color: sampleBooks[0].coverColor,
+          video: sampleBooks[0].videoUrl,
+        }}
+      />
+
+      {/* BOOK LIST */}
+      <BookList
+        title="Latest Books"
+        books={sampleBooks.map((book) => ({
+          ...book,
+          color: book.coverColor,
+          video: book.videoUrl,
+        }))}
+        containerClassname="mt-28"
+      />
+    </>
+  );
+};
 
 export default Home;
