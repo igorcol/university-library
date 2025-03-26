4977b10358235f1ca6a9fe48067e8c1184879d8d
igorcol <igor.colombini@gmail.com>
2025-03-26 19:20:50 -0300

    refact(lib/workflow): Fetching emailjs api

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,5 +1,4 @@
 import { Client as WorkflowClient } from '@upstash/workflow'
-import emailjs from '@emailjs/browser'
 import config from './config'
 
 export const workflowClient = new WorkflowClient({
@@ -9,17 +8,28 @@ export const workflowClient = new WorkflowClient({
 
 //* SEND EMAIL FUNCTION
 export const sendEmail = async ({ email, subject, message }: { email: string, subject: string, message: string }) => {
-    const templateParams = {
-        to_email: email,
-        subject: subject,
-        message: message
+    const payload = {
+        service_id: config.env.emailJS.serviceId,
+        template_id: config.env.emailJS.templateId,
+        user_id: config.env.emailJS.publicKey,
+        templateParams: {
+            to_email: email,
+            subject: subject,
+            message: message
+        }
     }
 
-    await emailjs.send(
-        config.env.emailJS.serviceId,
-        config.env.emailJS.templateId,
-        templateParams,
-        config.env.emailJS.publicKey
-    )
+    // Envia
+    const res = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    });
+
+    if (!res.ok) {
+        const errorText = await res.text();
+        console.error("X | EmailJS failed:", errorText);
+        throw new Error("X | EmailJS failed: " + errorText);
+    }
 }
 
