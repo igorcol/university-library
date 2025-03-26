f4f63801f15ff4e423ab9fb3d70fb49e9aa0ccf8
igorcol <igor.colombini@gmail.com>
2025-03-26 20:11:44 -0300

    refact: sendEmail payload

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -11,7 +11,8 @@ export const sendEmail = async ({ email, subject, message }: { email: string, su
     const payload = {
         service_id: config.env.emailJS.serviceId,
         template_id: config.env.emailJS.templateId,
-        //user_id: config.env.emailJS.publicKey,
+        user_id: config.env.emailJS.publicKey,
+        accessToken: config.env.emailJS.privateKey,
         template_params: {
             to_email: email,
             subject: subject,
@@ -20,13 +21,14 @@ export const sendEmail = async ({ email, subject, message }: { email: string, su
     }
     //console.log("...EMAIL PAYLOAD", JSON.stringify(payload, null, 2))
 
+
     // Envia
     const res = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
         method: 'POST',
-        headers: { 
+        headers: new Headers({ 
             "Content-Type": "application/json",
-            Authorization: `Bearer ${config.env.emailJS.privateKey}` 
-        },
+            //Authorization: `Bearer ${config.env.emailJS.privateKey}` 
+        }),
         body: JSON.stringify(payload)
     })
 
