745a650ee6565b081bb9d7eae7fc0d8bb448f1d5
igorcol <igor.colombini@gmail.com>
2025-03-26 19:53:29 -0300

    refact: POST with Authorization Berer

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -11,19 +11,22 @@ export const sendEmail = async ({ email, subject, message }: { email: string, su
     const payload = {
         service_id: config.env.emailJS.serviceId,
         template_id: config.env.emailJS.templateId,
-        user_id: config.env.emailJS.privateKey,
+        user_id: config.env.emailJS.publicKey,
         template_params: {
             to_email: email,
             subject: subject,
             message: message
         }
     }
-    console.log("...EMAIL PAYLOAD", JSON.stringify(payload, null, 2))
+    //console.log("...EMAIL PAYLOAD", JSON.stringify(payload, null, 2))
 
     // Envia
-    const res = await fetch("https://api.emailjs.com/api/v1.0/email/send-private", {
+    const res = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
         method: 'POST',
-        headers: { "Content-Type": "application/json" },
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${config.env.emailJS.privateKey}` 
+        },
         body: JSON.stringify(payload)
     })
 
