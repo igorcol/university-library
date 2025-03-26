c33059a948d2a3308c8bbeea64145ba20bb1fc66
igorcol <igor.colombini@gmail.com>
2025-03-26 18:59:46 -0300

    refact(lib/workflow): Implements EmailJS

--- lib/config.ts
+++ lib/config.ts
@@ -15,6 +15,11 @@ const config = {
             qstashToken: process.env.QSTASH_TOKEN!,
         },
         resendToken: process.env.RESEND_TOKEN!, // ! NÃO ESTA SENDO UTILIZADO
+        emailJS: {
+            serviceId: process.env.EMAILJS_SERVICE_ID!,
+            templateId: process.env.EMAILJS_TEMPLATE_ID!,
+            publicKey: process.env.EMAILJS_PUBLIC_KEY!
+        }
     }
 }
 
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,5 +1,5 @@
 import { Client as WorkflowClient } from '@upstash/workflow'
-import { Client as QStashClient, resend } from "@upstash/qstash";
+import emailjs from '@emailjs/browser'
 import config from './config'
 
 export const workflowClient = new WorkflowClient({
@@ -7,21 +7,19 @@ export const workflowClient = new WorkflowClient({
     token: config.env.upstash.qstashToken
 })
 
-const qstashClient = new QStashClient({ token: config.env.upstash.qstashToken });
-
 //* SEND EMAIL FUNCTION
 export const sendEmail = async ({ email, subject, message }: { email: string, subject: string, message: string }) => {
-    await qstashClient.publishJSON({
-        api: {
-            name: "email",
-            provider: resend({ token: config.env.resendToken }),
-        },
-        body: {
-            from: "BookWise <igor.colombini@gmail.com>",
-            to: [email],
-            subject: subject,
-            html: message,
-        },
-    });
+    const templateParams = {
+        to_email: email,
+        subject: subject,
+        message: message
+    }
+
+    await emailjs.send(
+        config.env.emailJS.serviceId,
+        config.env.emailJS.templateId,
+        templateParams,
+        config.env.emailJS.publicKey
+    )
 }
 
