a7a7fd3320b13a03131e4a95d32b68f001e8a5e3
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-26 21:44:34 -0300

    (admin) bookSchema

---
+++ app/admin/books/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full rounded-2xl bg-white p-7">
+      <div className="flex flex-wrap items-center justify-between ga-2">
+        <h2 className="text-xl font-semibold">All Books</h2>
+        <Button className="bg-primary-admin" asChild>
+          <Link href="/admin/books/new" className="text-light-100">+ Create a new Book</Link>
+        </Button>
+      </div>
+
+      <div className="mt-7 w-full overflow-hidden">
+        <p>Table</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- lib/validations.ts
+++ lib/validations.ts
@@ -12,3 +12,15 @@ export const signInSchema = z.object({
     email: z.string().email(),
     password: z.string().min(3)
 })
+
+export const bookSchema = z.object({
+    title: z.string().trim().min(2).max(100),
+    description: z.string().trim().min(10).max(2000),
+    author: z.string().trim().min(2).max(50),
+    rating: z.number().min(1).max(5),
+    totalCopies: z.coerce.number().int().positive().lte(10000),
+    coverUrl: z.string().nonempty(),
+    coverColor: z.string().trim().regex(/^#[a-fA-F0-9]{6}$/),
+    videoUrl: z.string().nonempty(),
+    summary: z.string().trim().min(10)
+})
