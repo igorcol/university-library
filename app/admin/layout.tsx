9dba7e38661e51b998e666fa81ea558644709355
igorcol <igor.colombini@gmail.com>
2025-03-27 11:55:49 -0300

    adm pages Role Verification

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,26 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
-import '@/styles/admin.css'
 import Sidebar from "@/components/admin/Sidebar";
 import Header from "@/components/admin/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import "@/styles/admin.css";
 
 const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  if (!session?.user?.id) redirect("/sign-in");
 
-    const session = await auth();
-    if(!session?.user?.id) redirect('/sign-in')
+  // Check if user is admin
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === 'ADMIN')
+
+  if (!isAdmin) redirect('/')
 
   return (
     <main className="flex min-h-screen w-full flex-row">
