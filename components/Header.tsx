31a8c1fe7b6f89eac31d9bd8ab64ad456d6824b9
igorcol <igor.colombini@gmail.com>
2025-03-27 15:59:29 -0300

    feat: adm-dashboard link in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,25 +6,31 @@ import { signOut } from "@/auth";
 
 const logoImg = "/icons/logo.svg";
 
-const Header = () => {
+const Header = async () => {
+
   return (
-    <header className="my-10 flex justify-between gap-5">
+    <header className="my-10 flex justify-between items-center">
       <Link href="/">
         <Image src={logoImg} alt="logo" width={40} height={40} />
       </Link>
 
       <ul className="flex flex-row items-center gap-8">
+          <li>
+            <Link href={"/admin"}>
+              <p className="text-light-100 underline">Admin Dashboard</p>
+            </Link>
+          </li>
+
         <li>
           <form
             action={async () => {
               "use server";
               await signOut();
             }}
-            className="mb-10"
           >
             <Button>Logout</Button>
           </form>
-          
+
           {/* <Link href="/my-profile">
             <Avatar>
               <AvatarFallback className="bg-amber-100 font-bold flex items-center justify-center w-full">
